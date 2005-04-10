4f2d2255c8a1e4575a0ba76d4d35b2877bde61b0
leonb <>
2005-04-10 23:12:00 +0000

    Update copyright lines.

--- src/operator.c
+++ src/operator.c
@@ -1,4 +1,4 @@
-/*  Copyright (c) 1987 Leon Bottou
+/*  Copyright (C) 1987- Leon Bottou
  * 
  *  This is free documentation; you can redistribute it and/or
  *  modify it under the terms of the GNU General Public License as
